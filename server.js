192337104a41304ebb0e7245e523865f0e528d97
Sascha Gros <gros.sascha@gmail.com>
2014-09-02 23:14:03 +0200

    Add user model and signup functionality

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var port    = process.env.PORT || 8080;
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
 
-// require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport')(passport); // pass passport for configuration
 
 // set up our express application
 app.use(morgan('dev')); // log every request to the console
