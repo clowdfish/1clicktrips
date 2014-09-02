bd8787ae176680b5d39af1e57e37f1cf3c1bf98b
Sascha Gros <gros.sascha@gmail.com>
2014-09-02 23:07:18 +0200

    Create basic views and routing

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+
+    'url' : 'mongodb://authServerAdmin:1234@localhost:27017/authserver'
+};
---
+++ server.js
@@ -0,0 +1,41 @@
+// set up ======================================================================
+var express  = require('express'),
+    mongoose = require('mongoose'),
+    passport = require('passport'),
+    flash 	 = require('connect-flash');
+
+var morgan       = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser   = require('body-parser'),
+    session      = require('express-session');
+
+var configDB     = require('./config/database.js');
+
+var app     = express();
+var port    = process.env.PORT || 8080;
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs as templating engine
+
+// required for passport
+app.use(session({ secret: 'theanswertoallquestions42.whatwas23for?' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('The magic happens on port ' + port);
+
