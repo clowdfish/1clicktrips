a6f8ea3d82776003aeb59c12eb153c261853edeb
Sascha Gros <gros.sascha@gmail.com>
2014-09-03 12:08:52 +0200

    Implement linking of accounts

--- app/routes.js
+++ app/routes.js
@@ -1,68 +1,64 @@
 module.exports = function(app, passport) {
 
-    // =====================================
+    // =============================================================================
+    // STANDARD ROUTES =============================================================
+    // =============================================================================
+
     // HOME PAGE (with login links) ========
-    // =====================================
     app.get('/', function(req, res) {
         res.render('index.ejs'); // load the index.ejs file
     });
 
-    // =====================================
-    // LOGIN ===============================
-    // =====================================
-    // show the login form
-    app.get('/login', function(req, res) {
-
-        // render the page and pass in any flash data if it exists
-        res.render('login.ejs', { message: req.flash('loginMessage') });
-    });
-
-    // process the login form
-    app.post('/login', passport.authenticate('local-login', {
-        successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/login', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
-
-    // =====================================
-    // SIGNUP ==============================
-    // =====================================
-    // show the signup form
-    app.get('/signup', function(req, res) {
-
-        // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', { message: req.flash('signupMessage') });
-    });
-
-    // process the signup form
-    app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/signup', // redirect back to the signup page if there is an error
-        failureFlash : true // allow flash messages
-    }));
-
-    // =====================================
-    // PROFILE SECTION =====================
-    // =====================================
-    // we will want this protected so you have to be logged in to visit
-    // we will use route middleware to verify this (the isLoggedIn function)
+    // PROFILE SECTION (PROTECTED) =========
     app.get('/profile', isLoggedIn, function(req, res) {
         res.render('profile.ejs', {
             user : req.user // get the user out of session and pass to template
         });
     });
 
-    // =====================================
     // LOGOUT ==============================
-    // =====================================
     app.get('/logout', function(req, res) {
         req.logout();
         res.redirect('/');
     });
 
-    // =====================================
-    // TWITTER ROUTES ======================
-    // =====================================
+    // =============================================================================
+    // AUTHENTICATE (FIRST LOGIN) ==================================================
+    // =============================================================================
+
+    // locally --------------------------------
+
+    // show the SIGNUP form
+    app.get('/signup', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    // process the SIGNUP form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    // LOGIN ===============================
+    // show the LOGIN form
+    app.get('/login', function(req, res) {
+
+        // render the page and pass in any flash data if it exists
+        res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
+
+    // process the LOGIN form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    // twitter --------------------------------
+
     // route for twitter authentication and login
     app.get('/auth/twitter', passport.authenticate('twitter'));
 
@@ -73,6 +69,50 @@ module.exports = function(app, passport) {
             failureRedirect : '/'
         }));
 
+    // =============================================================================
+    // AUTHORIZE (ALREADY LOGGED IN / CONNECTING OTHER SOCIAL ACCOUNT) =============
+    // =============================================================================
+
+    // locally --------------------------------
+
+    app.get('/connect/local', function(req, res) {
+        res.render('connect-local.ejs', { message: req.flash('loginMessage') });
+    });
+
+    app.post('/connect/local', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/connect/local', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
+
+    // twitter --------------------------------
+
+    // send to twitter to do the authentication
+    app.get('/connect/twitter', passport.authorize('twitter', { scope : 'email' }));
+
+    // handle the callback after twitter has authorized the user
+    app.get('/connect/twitter/callback',
+        passport.authorize('twitter', {
+            successRedirect : '/profile',
+            failureRedirect : '/'
+        }));
+
+    // =============================================================================
+    // UNLINK ACCOUNTS =============================================================
+    // =============================================================================
+    // used to unlink accounts. for social accounts, just remove the token
+    // for local account, remove email and password
+    // user account will stay active in case they want to reconnect in the future
+
+    // twitter --------------------------------
+    app.get('/unlink/twitter', function(req, res) {
+        var user           = req.user;
+        user.twitter.token = undefined;
+
+        user.save(function(err) {
+            res.redirect('/profile');
+        });
+    });
 };
 
 // route middleware to make sure a user is logged in
--- config/passport.js
+++ config/passport.js
@@ -39,46 +39,56 @@ module.exports = function(passport) {
             // by default, local strategy uses username and password, we will override with email
             usernameField : 'email',
             passwordField : 'password',
-            passReqToCallback : true // allows us to pass back the entire request to the callback
+            passReqToCallback : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
         },
         function(req, email, password, done) {
 
             // asynchronous
-            // User.findOne wont fire unless data is sent back
             process.nextTick(function() {
 
-                // find a user whose email is the same as the forms email
-                // we are checking to see if the user trying to login already exists
-                User.findOne({ 'local.email' :  email }, function(err, user) {
+                //  Whether we're signing up or connecting an account, we'll need
+                //  to know if the email address is in use.
+                User.findOne({'local.email': email}, function(err, existingUser) {
+
                     // if there are any errors, return the error
                     if (err)
                         return done(err);
 
-                    // check to see if theres already a user with that email
-                    if (user) {
+                    // check to see if there's already a user with that email
+                    if (existingUser)
                         return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-                    } else {
 
-                        // if there is no user with that email
+                    //  If we're logged in, we're connecting a new local account.
+                    if(req.user) {
+                        var user            = req.user;
+
+                        user.local.email    = email;
+                        user.local.password = user.generateHash(password);
+
+                        user.save(function(err) {
+                            if (err)
+                                throw err;
+                            return done(null, user);
+                        });
+                    }
+                    //  We're not logged in, so we're creating a brand new user.
+                    else {
                         // create the user
                         var newUser            = new User();
 
-                        // set the user's local credentials
                         newUser.local.email    = email;
                         newUser.local.password = newUser.generateHash(password);
 
-                        // save the user
                         newUser.save(function(err) {
                             if (err)
                                 throw err;
+
                             return done(null, newUser);
                         });
                     }
 
                 });
-
             });
-
         }));
 
     // =========================================================================
@@ -121,47 +131,80 @@ module.exports = function(passport) {
     // =========================================================================
     passport.use(new TwitterStrategy({
 
-            consumerKey     : configAuth.twitterAuth.consumerKey,
-            consumerSecret  : configAuth.twitterAuth.consumerSecret,
-            callbackURL     : configAuth.twitterAuth.callbackURL
-
+            consumerKey         : configAuth.twitterAuth.consumerKey,
+            consumerSecret      : configAuth.twitterAuth.consumerSecret,
+            callbackURL         : configAuth.twitterAuth.callbackURL,
+            passReqToCallback   : true // allows us to pass in the req from our route (lets us check if a user is logged in or not)
         },
-        function(token, tokenSecret, profile, done) {
+        function(req, token, tokenSecret, profile, done) {
 
             // make the code asynchronous
             // User.findOne won't fire until we have all our data back from Twitter
             process.nextTick(function() {
 
-                User.findOne({ 'twitter.id' : profile.id }, function(err, user) {
+                // check if the user is already logged in
+                if (!req.user) {
 
-                    // if there is an error, stop everything and return that
-                    // ie an error connecting to the database
-                    if (err)
-                        return done(err);
+                    User.findOne({ 'twitter.id': profile.id }, function (err, user) {
 
-                    // if the user is found then log them in
-                    if (user) {
-                        return done(null, user); // user found, return that user
-                    } else {
-                        // if there is no user, create them
-                        var newUser                 = new User();
+                        // if there is an error, stop everything and return that
+                        // ie an error connecting to the database
+                        if (err)
+                            return done(err);
 
-                        // set all of the user data that we need
-                        newUser.twitter.id          = profile.id;
-                        newUser.twitter.token       = token;
-                        newUser.twitter.username    = profile.username;
-                        newUser.twitter.displayName = profile.displayName;
+                        // if the user is found then log them in
+                        if (user) {
+                            // if there is a user id already but no token (user was linked at one point and then removed)
+                            // just add our token and profile information
+                            if (!user.twitter.token) {
+                                user.twitter.token = token;
+                                user.twitter.username = profile.username;
+                                user.twitter.displayName = profile.displayName;
 
-                        // save our user into the database
-                        newUser.save(function(err) {
-                            if (err)
-                                throw err;
-                            return done(null, newUser);
-                        });
-                    }
-                });
+                                user.save(function (err) {
+                                    if (err)
+                                        throw err;
+                                    return done(null, user);
+                                });
+                            }
 
+                            return done(null, user); // user found, return that user
+                        } else {
+                            // if there is no user, create them
+                            var newUser = new User();
+
+                            // set all of the user data that we need
+                            newUser.twitter.id = profile.id;
+                            newUser.twitter.token = token;
+                            newUser.twitter.username = profile.username;
+                            newUser.twitter.displayName = profile.displayName;
+
+                            // save our user into the database
+                            newUser.save(function (err) {
+                                if (err)
+                                    throw err;
+                                return done(null, newUser);
+                            });
+                        }
+                    });
+                }
+                else {
+                    // user already exists and is logged in, we have to link accounts
+                    var user = req.user; // pull the user out of the session
+
+                    // update the current users twitter credentials
+                    user.twitter.id             = profile.id;
+                    user.twitter.token          = token;
+                    user.twitter.username       = profile.username;
+                    user.twitter.displayName    = profile.displayName;
+
+                    // save the user
+                    user.save(function(err) {
+                        if (err)
+                            throw err;
+                        return done(null, user);
+                    });
+                }
             });
-
         }));
 };
