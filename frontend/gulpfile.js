453fd581eb3bb8ef04599ba0aedb4f5a061b8cbe
Sascha Gros <gros.sascha@gmail.com>
2015-01-27 09:45:46 +0100

    Strip down gulpfile and remove unnecessary node and bower modules

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -3,34 +3,33 @@ var gulp = require('gulp');
 
 // include core modules
 var path  = require("path");
-var fs = require("fs");
+var fs    = require("fs");
 
 // include gulp plug-ins
-var changed 	= require('gulp-changed'),
-    imagemin 	= require('gulp-imagemin'),
-    minifyHTML 	= require('gulp-minify-html'),
-    concat 		= require('gulp-concat'),
-    rename		= require('gulp-rename'),
+var changed 	  = require('gulp-changed'),
+    imagemin 	  = require('gulp-imagemin'),
+    concat 		  = require('gulp-concat'),
+    coffee      = require('gulp-coffee'),
     stripDebug 	= require('gulp-strip-debug'),
-    uglify 		= require('gulp-uglify'),
-    autoprefix 	= require('gulp-autoprefixer'),
+    uglify 		  = require('gulp-uglify'),
+    autoprefix  = require('gulp-autoprefixer'),
     minifyCSS 	= require('gulp-minify-css'),
-    sass 		= require('gulp-ruby-sass'),
-    notify		= require('gulp-notify'),
-    plumber 	= require('gulp-plumber'),
-    ts          = require('gulp-typescript'),
+    sass 		    = require('gulp-ruby-sass'),
+    notify		  = require('gulp-notify'),
+    plumber 	  = require('gulp-plumber'),
     webserver   = require('gulp-webserver'),
-    gulpif = require('gulp-if'),
-    declare = require('gulp-declare'),
-    wrap = require('gulp-wrap'),
-    yaml = require('gulp-yml'),
-    yml = require('js-yaml'),
-    compile = require('gulp-compile-handlebars'),
-    handlebars = require('gulp-handlebars');
+    gulpif      = require('gulp-if'),
+    declare     = require('gulp-declare'),
+    yaml        = require('gulp-yml'),
+    yml         = require('js-yaml'),
+    compile     = require('gulp-compile-handlebars');
 
 /****************************************************************************************************/
 /* SETTING UP DEVELOPMENT ENVIRONMENT                                                               */
 /****************************************************************************************************/
+
+// development/production flag
+var production = false;
 
 // the title and icon that will be used for notifications
 var notifyInfo = {
@@ -51,39 +50,31 @@ var plumberErrorHandler = {
 /* BUILD TASKS                                                                                      */
 /****************************************************************************************************/
 
-gulp.task('translate', function() {
-  var translations = ['de', 'en'];
-
-  translations.forEach(function(translation){
-    gulp.src('app/views/**/*.html')
-      .pipe(plumber(plumberErrorHandler))
-      .pipe(translate('./locales/'+ translation +'.yml'))
-      .pipe(gulp.dest('dist/views/' + translation));
-  });
-});
-
+// copy font-awesome and compile styles
 gulp.task('styles', function() {
-    gulp.src(['bower_components/font-awesome/fonts/*']).pipe(gulp.dest("build/fonts"));
-    gulp.src(['bower_components/messenger/build/css/messenger-theme-flat.css']).pipe(gulp.dest("build/styles"));
-    gulp.src(['bower_components/jt.timepicker/jquery.timepicker.css']).pipe(gulp.dest("build/styles"));
-    gulp.src(['bower_components/pikaday/css/pikaday.css']).pipe(gulp.dest("build/styles"));
-    return gulp.src(['styles/*.scss'])
-        .pipe(plumber(plumberErrorHandler))
-        .pipe(sass({ style: 'expanded' }))
-        //.pipe(autoprefix('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
-        .pipe(gulp.dest('build/styles'));
+  gulp.src(['bower_components/font-awesome/fonts/*'])
+    .pipe(gulp.dest("build/fonts"));
+
+  return gulp.src(['styles/*.scss'])
+    .pipe(plumber(plumberErrorHandler))
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(gulpif(production, autoprefix('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1')))
+    .pipe(gulpif(production, minifyCSS()))
+    .pipe(gulp.dest('build/styles'));
 });
 
+// minify images
 gulp.task('images', function() {
-    var imgSrc = 'images/**/*',
-        imgDst = 'build/images';
+  var imgSrc = 'images/**/*',
+      imgDst = 'build/images';
 
-    gulp.src(imgSrc)
-        .pipe(changed(imgDst))
-        .pipe(imagemin())
-        .pipe(gulp.dest(imgDst));
+  gulp.src(imgSrc)
+    .pipe(changed(imgDst))
+    .pipe(imagemin())
+    .pipe(gulp.dest(imgDst));
 });
 
+// start development web server
 gulp.task('webserver', function() {
   gulp.src('build/')
     .pipe(webserver({
@@ -95,61 +86,31 @@ gulp.task('webserver', function() {
     }));
 });
 
-gulp.task('scripts', ['i18n', 'templates'], function() {
+// process and compile all script files
+gulp.task('scripts', ['i18n'], function() {
   gulp.src(
     [
-      'bower_components/handlebars/handlebars.runtime.js',
-      'bower_components/jquery/dist/jquery.js',
-      'bower_components/velocity/velocity.js',
-      'bower_components/jt.timepicker/jquery.timepicker.js',
-      'bower_components/Sortable/Sortable.js',
+      'bower_components/angular/angular.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
       'bower_components/underscore/underscore.js',
-      'bower_components/backbone/backbone.js',
       'bower_components/mediator-js/lib/mediator.js',
-      'bower_components/moment/moment.js',
-      'bower_components/pikaday/pikaday.js',
-      'bower_components/v.js/V.js',
-      'scripts/V.ts',
-      'scripts/locales.js',
-      'scripts/build/templates.js',
-      'scripts/ajax.ts',
-      'scripts/utils.ts',
-      'scripts/models/*.ts',
-      'scripts/controllers/*.ts',
-      'scripts/views/*.ts',
-      'scripts/app.ts',
-      'scripts/main.ts'
+      'bower_components/moment/moment.js'
     ]
-  ).pipe(plumber(plumberErrorHandler))
-  .pipe(
-    gulpif(
-      /[.]ts$/,
-      ts({
-        declarationFiles: false,
-        noExternalResolve: false,
-        target: "ES5"
-      })
-    )
   )
-  .pipe(concat('script.js'))
-  //.pipe(stripDebug())
-  //.pipe(gulpif(production, uglify()))
-  .pipe(gulp.dest('build/scripts'));
-});
-
-gulp.task('templates', function() {
-  gulp.src('templates/dynamic/*.hbs')
     .pipe(plumber(plumberErrorHandler))
-    .pipe(handlebars())
-    .pipe(wrap('Handlebars.template(<%= contents %>)'))
-    .pipe(declare({
-      namespace: 'Tmpl',
-      noRedeclare: true // Avoid duplicate declarations
-    }))
-    .pipe(concat('templates.js'))
-    .pipe(gulp.dest('scripts/build/'));
+    .pipe(
+      gulpif(
+        /[.]coffee$/,
+        coffee({bare: true})
+      )
+    )
+    .pipe(concat('script.js'))
+    .pipe(gulpif(production, stripDebug()))
+    .pipe(gulpif(production, uglify()))
+    .pipe(gulp.dest('build/scripts'));
 });
 
+// compile html files and replace language strings
 gulp.task('preprocess', function() {
   var locales = ['en', 'de'];
 
@@ -157,23 +118,26 @@ gulp.task('preprocess', function() {
     var templateData = {
       T: yml.safeLoad(fs.readFileSync('./i18n/' + locale + '.yaml', 'utf8'))[locale],
       locale: locale,
-      production: true
+      production: production
     },
     options = {
       batch : ['templates/modules', 'templates/partials'],
       helpers : {
-          capitals : function(str){
-              return str.toUpperCase();
-          }
+        capitals : function(str) {
+            return str.toUpperCase();
+        }
       }
-    }
+    };
+
     gulp.src('templates/*.html')
-    .pipe(plumber(plumberErrorHandler))
-    .pipe(compile(templateData, options))
-    .pipe(gulp.dest(path.join('build/', locale)));
+      .pipe(plumber(plumberErrorHandler))
+      .pipe(compile(templateData, options))
+      .pipe(gulp.dest(path.join('build/', locale)));
   });
 });
 
+
+// create translation file
 gulp.task('i18n', function() {
   gulp.src('i18n/*.yaml')
     .pipe(plumber(plumberErrorHandler))
@@ -187,11 +151,10 @@ gulp.task('i18n', function() {
     .pipe(gulp.dest('scripts/build/'));
 });
 
-// DEVELOPMENT
-
+// gulp task suite
 gulp.task('live', ['styles', 'scripts', 'images', 'preprocess', 'webserver'], function() {
   gulp.watch('styles/**/*.scss', ['styles']);
-  gulp.watch(['templates/**/*.hbs', 'scripts/**/*.ts'], ['scripts']);
+  gulp.watch(['scripts/**/*.coffee'], ['scripts']);
   gulp.watch(['templates/**/*.html', 'i18n/*.yaml'], ['preprocess']);
 });
 
--- frontend/scripts/typedefs/mediator.d.ts
+++
@@ -1,5 +0,0 @@
-declare class Mediator {
-  subscribe(channel: string, callback: (...args: any[]) => any, options?: any, context?: any);
-  publish(channel: string, ...data: any[]);
-  remove(channel: string, identifier?: string);
-}
