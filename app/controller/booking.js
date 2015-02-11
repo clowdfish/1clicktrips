411c20eb6bca251fb95829e87ae89af951319a51
Sascha Gros <gros.sascha@gmail.com>
2015-02-11 09:51:16 +0100

    Add booking resource to API

---
+++ app/controller/booking.js
@@ -0,0 +1,18 @@
+// user.js
+var dbConfig = require('../../config/database.js');
+var Promise = require('es6-promise').Promise;
+
+module.exports = {
+
+  getBookings: function(userId) {
+    // TODO implement
+  },
+
+  makeBooking: function(userId, bookingObject) {
+    // TODO implement
+  },
+
+  getById: function(bookingId) {
+    // TODO implement
+  }
+};
