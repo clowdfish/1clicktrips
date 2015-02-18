2785b3beb0a83a0d23aca88036b9f1dfd661b070
Nam Tran <nam.trankhanh.vn@gmail.com>
2015-02-18 09:58:44 +0700

    add test for dashboard service

---
+++ frontend/scripts/app/test/dashboard/bookingService.spec.js
@@ -0,0 +1,33 @@
+'use strict';
+
+describe('bookingService', function() {
+  var bookingService,
+      $rootScope,
+      $q;
+  beforeEach(module('app.dashboard'));
+  beforeEach(inject(function(_bookingService_,
+                            _$rootScope_,
+                            _$q_,
+                            _mockBooking_) {
+    $rootScope = _$rootScope_;
+    bookingService = _bookingService_;
+    $q = _$q_;
+
+    spyOn(bookingService, 'getBookedTrips').and.callFake(function() {
+      var deferred = $q.defer();
+      deferred.resolve(_mockBooking_);
+      return deferred.promise;
+    })
+  }));
+
+  it('get booked trip and return data', function() {
+    var bookedTrips = [];
+    bookingService
+      .getBookedTrips()
+      .then(function(data) {
+        bookedTrips = data;
+      });
+    $rootScope.$digest();
+    expect(bookedTrips.length).toBeGreaterThan(0);
+  });
+});
---
+++ frontend/scripts/app/test/dashboard/favoriteService.spec.js
@@ -0,0 +1,38 @@
+'use strict';
+
+describe('favoriteService', function() {
+  var favoriteService,
+      $q,
+      $rootScope;
+  beforeEach(module('app.dashboard'));
+  beforeEach(inject(function(_$rootScope_,
+                            _favoriteService_,
+                            _$q_,
+                            _mockFavorites_) {
+    $rootScope = _$rootScope_;
+    favoriteService = _favoriteService_;
+    $q = _$q_;
+
+    spyOn(favoriteService, 'getFarvoriteList')
+      .and.callFake(function() {
+        var deferred = $q.defer();
+        deferred.resolve(_mockFavorites_);
+        return deferred.promise;
+      });
+
+  }));
+
+  it('get favorite list', function() {
+    var favorites = [];
+    favoriteService
+      .getFarvoriteList()
+      .then(function(data){
+        favorites = data;
+      });
+
+    $rootScope.$digest();
+
+    expect(favorites.length).toEqual(3);
+    expect(favorites[0].origin.description).toEqual("9579 Quaking Hill, Panther Valley, Indiana");
+  });
+});
