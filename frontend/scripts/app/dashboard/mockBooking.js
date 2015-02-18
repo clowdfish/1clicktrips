b10689720aa15a4bbc97c226691f9c59d80d886d
Nam Tran <nam.trankhanh.vn@gmail.com>
2015-02-18 09:48:18 +0700

    add mockdata for dashboard test

---
+++ frontend/scripts/app/dashboard/mockBooking.js
@@ -0,0 +1,144 @@
+(function() {
+  angular
+    .module('app.dashboard')
+    .value('mockBooking', [
+      {
+        "id": 1,
+        "title": "3rd Phoenix World IT Conference ",
+        "destination": "Double Tree by Hilton Metropolitan, New York, USA",
+        "tags": [
+          "test", "another", "bla"
+        ],
+        "startDate": "2015-02-09T02:54:51+00:0",
+        "endDate": "2015-02-10T02:54:51+00:0",
+        "reference": "http://somewhere.com",
+        "hotel": {
+          "name": "Double Tree by Hilton Metropolitan",
+          "checkIn": "2015-02-10T02:54:51+00:0",
+          "url": "http://somewhere.com",
+          "image": "http://placehold.it/150x150"
+        },
+        "participants": [
+          {
+            "firstName": "Eric",
+            "lastName": "Conan",
+            "email": "eric@conan.com",
+            "booked": true,
+            "arrivalTime": "2015-02-09T02:54:51+00:0",
+            "image": "http://placehold.it/150x150",
+            "profile": "http://somewhere.com"
+          },
+          {
+            "firstName": "Jenny",
+            "lastName": "Kandone",
+            "email": "jenny@kandone.com",
+            "booked": true,
+            "arrivalTime": "2015-02-09T02:54:51+00:0",
+            "image": "http://placehold.it/150x150",
+            "profile": "http://somewhere.com"
+          },
+          {
+            "firstName": "Alvin",
+            "lastName": "Cooke",
+            "email": "alvin@cooke.com",
+            "booked": false,
+            "arrivalTime": "2015-02-09T02:54:51+00:0",
+            "image": "http://placehold.it/150x150",
+            "profile": "http://somewhere.com"
+          }
+        ]
+      },
+      {
+        "id": 2,
+        "title": "3rd Phoenix World IT Conference ",
+        "destination": "Double Tree by Hilton Metropolitan, New York, USA",
+        "tags": [
+          "test", "another", "bla"
+        ],
+        "startDate": "2015-02-09T02:54:51+00:0",
+        "endDate": "2015-02-10T02:54:51+00:0",
+        "reference": "http://somewhere.com",
+        "hotel": {
+          "name": "Double Tree by Hilton Metropolitan",
+          "checkIn": "2015-02-10T02:54:51+00:0",
+          "url": "http://somewhere.com",
+          "image": "http://placehold.it/150x150"
+        },
+        "participants": [
+          {
+            "firstName": "Eric",
+            "lastName": "Conan",
+            "email": "eric@conan.com",
+            "booked": true,
+            "arrivalTime": "2015-02-09T02:54:51+00:0",
+            "image": "http://placehold.it/150x150",
+            "profile": "http://somewhere.com"
+          },
+          {
+            "firstName": "Jenny",
+            "lastName": "Kandone",
+            "email": "jenny@kandone.com",
+            "booked": true,
+            "arrivalTime": "2015-02-09T02:54:51+00:0",
+            "image": "http://placehold.it/150x150",
+            "profile": "http://somewhere.com"
+          },
+          {
+            "firstName": "Alvin",
+            "lastName": "Cooke",
+            "email": "alvin@cooke.com",
+            "booked": false,
+            "arrivalTime": "2015-02-09T02:54:51+00:0",
+            "image": "http://placehold.it/150x150",
+            "profile": "http://somewhere.com"
+          }
+        ]
+      },
+      {
+        "id": 3,
+        "title": "3rd Phoenix World IT Conference ",
+        "destination": "Double Tree by Hilton Metropolitan, New York, USA",
+        "tags": [
+          "test", "another", "bla"
+        ],
+        "startDate": "2015-02-09T02:54:51+00:0",
+        "endDate": "2015-02-10T02:54:51+00:0",
+        "reference": "http://somewhere.com",
+        "hotel": {
+          "name": "Double Tree by Hilton Metropolitan",
+          "checkIn": "2015-02-10T02:54:51+00:0",
+          "url": "http://somewhere.com",
+          "image": "http://placehold.it/150x150"
+        },
+        "participants": [
+          {
+            "firstName": "Eric",
+            "lastName": "Conan",
+            "email": "eric@conan.com",
+            "booked": true,
+            "arrivalTime": "2015-02-09T02:54:51+00:0",
+            "image": "http://placehold.it/150x150",
+            "profile": "http://somewhere.com"
+          },
+          {
+            "firstName": "Jenny",
+            "lastName": "Kandone",
+            "email": "jenny@kandone.com",
+            "booked": true,
+            "arrivalTime": "2015-02-09T02:54:51+00:0",
+            "image": "http://placehold.it/150x150",
+            "profile": "http://somewhere.com"
+          },
+          {
+            "firstName": "Alvin",
+            "lastName": "Cooke",
+            "email": "alvin@cooke.com",
+            "booked": false,
+            "arrivalTime": "2015-02-09T02:54:51+00:0",
+            "image": "http://placehold.it/150x150",
+            "profile": "http://somewhere.com"
+          }
+        ]
+      }
+    ]);
+})();
---
+++ frontend/scripts/app/dashboard/mockFavorites.js
@@ -0,0 +1,57 @@
+(function() {
+  angular
+    .module('app.dashboard')
+    .value('mockFavorites', [
+      {
+        "id" : 1,
+        "origin" : {
+          "description" : "9579 Quaking Hill, Panther Valley, Indiana",
+          "location" : {
+            "latitude" : 48.709008,
+            "longitude" : 9.457281
+          }
+        },
+        "destination" : {
+          "description" : "Double Tree by Hilton Metropolitan, New York, USA",
+          "location" : {
+            "latitude" : 36.161805,
+            "longitude" : -115.141183
+          }
+        }
+      },
+      {
+        "id" : 2,
+        "origin" : {
+          "description" : "9579 Quaking Hill, Panther Valley, Indiana",
+          "location" : {
+            "latitude" : 48.709008,
+            "longitude" : 9.457281
+          }
+        },
+        "destination" : {
+          "description" : "Double Tree by Hilton Metropolitan, New York, USA",
+          "location" : {
+            "latitude" : 36.161805,
+            "longitude" : -115.141183
+          }
+        }
+      },
+      {
+        "id" : 3,
+        "origin" : {
+          "description" : "9579 Quaking Hill, Panther Valley, Indiana",
+          "location" : {
+            "latitude" : 48.709008,
+            "longitude" : 9.457281
+          }
+        },
+        "destination" : {
+          "description" : "Double Tree by Hilton Metropolitan, New York, USA",
+          "location" : {
+            "latitude" : 36.161805,
+            "longitude" : -115.141183
+          }
+        }
+      }
+    ]);
+})();
