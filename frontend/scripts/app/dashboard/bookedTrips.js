861c2169c5e050b7d37341ed97ec4880fc4be8f1
Sascha Gros <gros.sascha@gmail.com>
2015-03-13 12:22:03 +0100

    Improve styling of dashboard

--- frontend/scripts/app/dashboard/bookedTrips.js
+++ frontend/scripts/app/dashboard/bookedTrips.js
@@ -14,35 +14,48 @@
       link: link
     };
 
-    function link(scope, elements, attrs) {
-      initialize();
+    function link(scope, element, attrs) {
+      if ( scope.itemPerPage <= 0) {
+        throw new Error('Invalid item per page');
+      }
 
-      scope.$watch('listItems', function() {
-        initialize();
-      });
+      scope.slideDirection = 'slide-left';
+      scope.nextPage = nextPage;
+      scope.previousPage = previousPage;
 
       scope.$watch('currentPage', function() {
         scope.offset = (scope.currentPage - 1) * scope.itemPerPage;
       });
 
-      scope.changePage = changePage;
-
-      function initialize() {
-        if (scope.listItems.length == 0) {
-          return;
-        }
-        scope.currentPage = 1;
+      scope.$watch('listItems', function() {
         scope.itemPerPage = parseInt(scope.itemPerPage);
         scope.totalPage = Math.round(scope.listItems.length / scope.itemPerPage);
-        scope.offset = (scope.currentPage - 1) * scope.itemPerPage;
-      }
+        scope.currentPage = 1;
+        scope.disablePreviousPageButton = true;
+        scope.disableNextPageButton = scope.totalPage == 1;
+      });
 
-      function changePage(number) {
-        if (number > 0 && number <= scope.totalPage) {
-          scope.currentPage = number;
+      function nextPage() {
+        if (scope.currentPage < scope.totalPage) {
+          scope.currentPage++;
+          scope.slideDirection = 'slide-left';
+        }
+        if (scope.currentPage == scope.totalPage) {
+          scope.disableNextPageButton = true;
+          scope.disablePreviousPageButton = false;
         }
       }
 
+      function previousPage() {
+        if (scope.currentPage > 1) {
+          scope.currentPage--;
+          scope.slideDirection = 'slide-right'
+        }
+        if (scope.currentPage == 1) {
+          scope.disablePreviousPageButton = true;
+          scope.disableNextPageButton = false;
+        }
+      }
     }
   }
 })();
