4e13d455ff0e8d5ed3887f6c91317e21e4fa315d
Nam Tran <nam.trankhanh.vn@gmail.com>
2015-03-13 11:20:33 +0700

    add missing file for datepicker

---
+++ frontend/scripts/app/common/directives/datepickerPopup.js
@@ -0,0 +1,25 @@
+/**
+ * Work around for default date in date picker
+ */
+(function() {
+
+  'use strict';
+
+  angular
+    .module('app.common')
+    .directive('datepickerPopup', datepickerPopup);
+
+  function datepickerPopup(dateFilter, datepickerPopupConfig) {
+    return {
+      restrict: 'A',
+      priority: 1,
+      require: 'ngModel',
+      link: function(scope, element, attr, ngModel) {
+        var dateFormat = attr.datepickerPopup || datepickerPopupConfig.datepickerPopup;
+        ngModel.$formatters.push(function (value) {
+          return dateFilter(value, dateFormat);
+        });
+      }
+    };
+  };
+})();
