2dc1aef407e8bb7a9d6943dcef98fda79cada5a7
Nam Tran <nam.trankhanh.vn@gmail.com>
2015-05-08 16:25:36 +0700

    #119 remove hardcode bookable value

--- frontend/scripts/app/result/tripApi.service.js
+++ frontend/scripts/app/result/tripApi.service.js
@@ -392,7 +392,6 @@
         for (i = 0; i < itinerary.outbound.segments.length; i++) {
           segment = itinerary.outbound.segments[i];
           segment['tripId'] = itinerary.outbound.id;
-          segment['bookable'] = true;
 
           if (result[day] == null) {
             result[day] = [];
