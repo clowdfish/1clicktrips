22dd504027502dbd666d79eccc3db628e82fdc78
Sascha Gros <gros.sascha@gmail.com>
2015-05-19 15:10:59 +0200

    #151 Fix bug with missing Moment library

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -110,7 +110,7 @@ gulp.task('scripts', ['i18n'], function() {
     'bower_components/angular-ui-bootstrap-bower/ui-bootstrap-tpls.js',
     'bower_components/use-angular-translate/src/**/*.js',
     'bower_components/angular-animate/angular-animate.js',
-    'bower_components/moment/min/moment.js',
+    'bower_components/moment/moment.js',
     'bower_components/lodash/lodash.js',
     'bower_components/use-angular-translate/src/**/*.js',
     'bower_components/angular-local-storage/dist/angular-local-storage.js',
