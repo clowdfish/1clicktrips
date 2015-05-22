cc396e3ad927109e37136ae4e956760193929f32
Sascha Gros <gros.sascha@gmail.com>
2015-05-22 08:37:45 +0200

    #177 Add mock data for event and meeting space api

--- app/controller/searchController.js
+++ app/controller/searchController.js
@@ -6,6 +6,8 @@ var Config = require('../../config/general');
 var SearchEngine = require('ttg-search');
 var SearchApi = SearchEngine.SearchApi;
 
+var MockController = require('../mocking/searchController');
+
 // handle different timezones?
 var disableTimezones = true;
 var searchApi = new SearchApi(disableTimezones, Config.logLocation);
@@ -13,23 +15,19 @@ var searchApi = new SearchApi(disableTimezones, Config.logLocation);
 module.exports = {
 
   getEvents: function(filter, limit) {
-    console.log("Retrieving events. Limit=" + limit);
+    console.log("Retrieving mock events. Limit=" + limit);
 
     // TODO implement
 
-    return new Promise(function(resolve) {
-      resolve([]);
-    });
+    return MockController.getEvents(filter, limit);
   },
 
   getMeetingSpaces: function(filter, limit) {
-    console.log("Retrieving meeting spaces. Limit=" + limit);
+    console.log("Retrieving mock meeting spaces. Limit=" + limit);
 
     // TODO implement
 
-    return new Promise(function(resolve) {
-      resolve([]);
-    });
+    return MockController.getMeetingSpaces(filter, limit);
   },
 
   getAlternatives: function(tripId, segmentId, language, currency) {
@@ -49,33 +47,11 @@ module.exports = {
   },
 
   getAlternativeHotels: function(tripId, segmentId, language, currency) {
-    return new Promise(function(resolve, reject) {
-      var mockData = [{
-        "id": 1,
-        "name": "The New Yorker hotel",
-        "imageUrl": 'http://www.radissonblu.com/images/hotel-ghaziabad/1369345068525.jpg',
-        "standard": 5,
-        "distance": 1.5,
-        "price": 50
-      },
-      {
-        "id": 2,
-        "name": "Empire Hotel",
-        "imageUrl": 'http://www.radissonblu.com/images/hotel-ghaziabad/1369345068525.jpg',
-        "standard": 4,
-        "distance": 2.5,
-        "price": 30
-      },
-      {
-        "id": 3,
-        "name": "Mave Hotel NYC",
-        "imageUrl": 'http://www.radissonblu.com/images/hotel-ghaziabad/1369345068525.jpg',
-        "standard": 3,
-        "distance": 4,
-        "price": 10
-      }];
-      resolve(mockData);
-    });
+    console.log("Retrieving mock hotel alternatives.");
+
+    // TODO implement
+
+    return MockController.getAlternativeHotels(tripId, segmentId, language, currency);
   },
 
   getTripResults: function(searchObject, userLicence) {
