efa3a7021685d31ed87ef2eb69521e7631f4836b
Nam Tran <nam.trankhanh.vn@gmail.com>
2015-10-07 10:35:02 +0700

    #268 unit test for itinerary helper

--- frontend/scripts/test/_test.ts
+++ frontend/scripts/test/_test.ts
@@ -3,5 +3,8 @@
 /// <reference path="common/directives/waitingAnimation.directive.spec.ts" />
 /// <reference path="search/search.controller.spec.ts" />
 /// <reference path="search/suggestionAdapter.service.spec.ts" />
+/// <reference path="result/itineraryHelper.service.spec.ts" />
+
 /// <reference path="mocks/_mocks.ts" />
 /// <reference path="mocks/mockAddress.value.ts" />
+/// <reference path="mocks/mockItineraries.value.ts" />
