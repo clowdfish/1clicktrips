4b4e0505545af3e12ed4048cc2f34c1c3e2e2e34
Nam Tran <nam.trankhanh.vn@gmail.com>
2015-10-19 10:12:37 +0700

    #256 add unit test for language service

---
+++ frontend/scripts/test/common/services/language.service.spec.ts
@@ -0,0 +1,54 @@
+/// <reference path="../../_test.ts" />
+
+'use strict';
+
+describe('service: language', () => {
+  var language: Common.Language,
+      $q,
+      $localStorage,
+      $translate,
+      $scope,
+      languages: Array<Common.LanguageItem>;
+
+  beforeEach(angular.mock.module('app'));
+
+  beforeEach(inject((_$translate_,
+                     _$localStorage_,
+                     _$q_,
+                     _$rootScope_) => {
+    $q = _$q_;
+    $translate = _$translate_;
+    $localStorage = _$localStorage_;
+
+    if ($localStorage['language']) {
+      $localStorage['language'] = null;
+    }
+
+    $scope = _$rootScope_.$new();
+
+    language = new Common.Language($localStorage, $translate, $q);
+    languages = window['AppData']['languages'];
+    language.initialize();
+  }));
+
+  afterEach(() => {
+    if ($localStorage['language']) {
+      $localStorage['language'] = null;
+    }
+  });
+
+  it('English is default language', () => {
+    var defaultLanguage = _.find(languages, (item: Common.LanguageItem) => {
+      return item.isDefault === true;
+    });
+    expect(language.getActiveLanguage()).toEqual(defaultLanguage);
+  });
+
+  it('Get and set language correctly', () => {
+    language.changeLanguage('de').then(() => {
+
+    });
+    $scope.$digest();
+    expect(language.getActiveLanguage()['code']).toEqual('de');
+  });
+});
