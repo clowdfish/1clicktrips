7248a552bfac8e1000ef29604bdb35ec47a591dd
Nam Tran <nam.trankhanh.vn@gmail.com>
2015-10-20 07:58:11 +0700

    merge

--- frontend/scripts/app/search/_search.ts
+++ frontend/scripts/app/search/_search.ts
@@ -13,7 +13,8 @@ module Search {
     .directive('popupTimePicker', popupTimePicker)
     .directive('popupTimePickerWrapper', popupTimePickerWrapper)
     .config(decorateDatePicker)
-    .config(decoratePopupDatePicker);
+    .config(decoratePopupDatePicker)
+    .config(decorateTimePicker);
 
   function decorateDatePicker($provide) {
     $provide.decorator('daypickerDirective', function($delegate) {
@@ -27,6 +28,14 @@ module Search {
     $provide.decorator('datepickerPopupWrapDirective', function($delegate) {
       var directive = $delegate[0];
       directive.templateUrl = 'scripts/app/templates/search/datepicker/popup.html';
+      return $delegate;
+    });
+  }
+
+  function decorateTimePicker($provide) {
+    $provide.decorator('timepickerDirective', function($delegate) {
+      var directive = $delegate[0];
+      directive.templateUrl = 'scripts/app/templates/search/timepicker.html';
       return $delegate;
     });
   }
