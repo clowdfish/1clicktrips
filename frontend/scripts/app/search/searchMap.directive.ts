4b414fd6eeda05ddfa21d8785551976892f99eb1
Nam Tran <nam.trankhanh.vn@gmail.com>
2015-10-22 12:40:03 +0700

    #280 move magnifier when hover

--- frontend/scripts/app/search/searchMap.directive.ts
+++ frontend/scripts/app/search/searchMap.directive.ts
@@ -16,57 +16,94 @@ module Search {
 
     function link(scope, element, attrs) {
       var $element = $(element);
+      var context = null;
+      var mapImage:any = null;
       initializeStaticMap();
+      initializeMagnifierDragAndDrop();
 
       function initializeStaticMap() {
-        var mapImage:any =
+        mapImage =
           $('<img />').attr('src', null);
 
         $element.find('.map-view').append(mapImage);
 
         var innerCircle:any = $element.find('.inner-circle')[0];
-        var context = innerCircle.getContext("2d");
+        context = innerCircle.getContext("2d");
 
         // render default location
-        drawMap(mapImage, DEFAULT_LOCATION, context);
+        drawMap($element, mapImage, DEFAULT_LOCATION, context);
 
         scope.$watch('location', () => {
-          drawMap(mapImage, scope.location, context);
+          drawMap($element, mapImage, scope.location, context);
         });
+      }
+
+      function initializeMagnifierDragAndDrop() {
+        var $magnifier = $element.find('.magnifier');
+
+        var $container = $('.search-map-container');
+        var startDragAndDrop = false;
+        var startX = null;
+
+        $container.mousemove((e) => {
+          var magnifierWidth = $magnifier.width();
+          var magnifierPosition = $magnifier.position();
+          var containerWidth = $container.width()
+          var pos = e.clientX - $container.offset().left - magnifierWidth / 2;
+
+          if (pos <= 0) {
+            pos = 0;
+          }
+          if (pos + magnifierWidth > containerWidth) {
+            pos = containerWidth - magnifierWidth;
+          }
+
+          $magnifier.css({
+            left: pos
+          });
+          drawMiniMap(pos);
+        });
+      }
+
+      /**
+      * Draw static Google Maps image.
+      *
+      * @param mapImage
+      * @param location
+      * @param context
+      */
+      function drawMap($element,
+                      mapImage,
+                      location:any,
+                      context) {
+
+        if (location && location.longitude && location.latitude) {
+          var mapUrl = 'https://maps.googleapis.com/maps/api/staticmap?';
+          mapUrl += 'center=' + location.latitude + ',' + location.longitude;
+          mapUrl += '&size=880x120';
+          mapUrl += '&zoom=10'; // 15
+          mapUrl += '&scale=2'; // 2
+
+          mapImage.attr('src', mapUrl);
+          mapImage.load(() => {
+            var left = $element.find('.magnifier').position().left;
+            drawMiniMap(left);
+          });
+        }
+      }
+
+      function drawMiniMap(positionX) {
+        var sy = 20;
+        var sw = 310;
+        var sh = 310;
+        var dx = 0;
+        var dy = 0;
+        var dw = 510;
+        var dh = 410;
+        context.drawImage(mapImage[0], positionX, sy, sw, sh, dx, dy, dw, dh);
       }
     }
 
-    /**
-     * Draw static Google Maps image.
-     *
-     * @param mapImage
-     * @param location
-     * @param context
-     */
-    function drawMap(mapImage,
-                     location:any,
-                     context) {
 
-      if (location && location.longitude && location.latitude) {
-        var mapUrl = 'https://maps.googleapis.com/maps/api/staticmap?';
-        mapUrl += 'center=' + location.latitude + ',' + location.longitude;
-        mapUrl += '&size=880x120';
-        mapUrl += '&zoom=10'; // 15
-        mapUrl += '&scale=2'; // 2
-
-        mapImage.attr('src', mapUrl);
-        mapImage.load(() => {
-          var sx = 20;
-          var sy = 20;
-          var sw = 210;
-          var sh = 210;
-          var dx = 0;
-          var dy = 0;
-          var dw = 410;
-          var dh = 410;
-          context.drawImage(mapImage[0], sx, sy, sw, sh, dx, dy, dw, dh);
-        });
-      }
-    }
   }
 }
